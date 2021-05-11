64886b15224033613113946a4582175777177e95
Nguyen Tung Lam <nguyentunglamchv@gmail.com>
2021-05-11 16:26:33 +0700

    lam

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace QLTiemChung
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello");
+        }
     }
 }
