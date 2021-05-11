6fe9ad7fad5b4309ed77d7a153b020bb48bd3e53
long1382k <longlgviet@gmail.com>
2021-05-11 15:55:25 +0700

    Long

---
+++ code/FormBacSi.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using System.Data.SqlClient;
+using DevExpress.XtraReports.UI;
+
+namespace DXApplication2
+{
+     public partial class FormBacSi : Form
+     {
+          string flag;
+          public FormBacSi()
+          {
+               InitializeComponent();
+               dataBacSi.ReadOnly = true;
+          }
+
+          private void FormBacSi_Load(object sender, EventArgs e)
+          {
+               layoutThongTinBS.Enabled = false;
+               btnLuuBS.Enabled = false;
+               getData();
+          }
+         
+          public void getData()
+          {
+               KetNoi kn = new KetNoi();
+               dataBacSi.DataSource = kn.LoadData("HienThiDSBacSi");
+               for (int i = 0; i < dataBacSi.Rows.Count - 1; i++)
+               {
+                    dataBacSi.Rows[i].Cells[0].Value = i + 1;
+               }
+               kn.OpenConnection();
+          }
+          public void getDataTimKiem()
+          {
+               KetNoi kn = new KetNoi();
+               dataBacSi.DataSource = kn.LoadData("TimKiemBS");
+               for (int i = 0; i < dataBacSi.Rows.Count - 1; i++)
+               {
+                    dataBacSi.Rows[i].Cells[0].Value = i + 1;
+               }
+               kn.OpenConnection();
+          }
+          public void TaoMaBSMoi()
+          {
+               KetNoi kn = new KetNoi();
+               DataTable dt = kn.LoadData("HienThiDSBacSi");
+               int count = 0;
+               count = dt.Rows.Count;
+               string s1 = "";
+               int s2 = 0;
+               s1 = Convert.ToString(dt.Rows[count - 1][0].ToString());
+               s2 = Convert.ToInt32((s1.Remove(0, 2)));
+               if (s2 + 1 < 10)
+                    txtMaBS.Text = "BS00" + (s2 + 1).ToString();
+               else if (s2 + 1 < 100)
+                    txtMaBS.Text = "BS0" + (s2 + 1).ToString();
+               else if (s2 + 1 < 1000)
+                    txtMaBS.Text = "BS" + (s2 + 1).ToString();
+               else
+                    txtMaBS.Text = "";
+          }
+          public void khoa()
+
+          {
+               btnThemBS.Enabled = true;
+               btnSuaBS.Enabled = true;
+               btnXoaBS.Enabled = true;
+               btnLuuBS.Enabled = false;
+               btnHuyBS.Enabled = false;
+               layoutThongTinBS.Enabled = false;
+          }
+
+
+
+          public void mo()
+          {
+               btnThemBS.Enabled = false;
+               btnSuaBS.Enabled = false;
+               btnXoaBS.Enabled = false;
+               btnLuuBS.Enabled = true;
+               btnHuyBS.Enabled = true;
+               layoutThongTinBS.Enabled = true;
+               txtMaBS.Focus();
+          }
+          public bool check()
+          {
+               if (string.IsNullOrWhiteSpace(txtMaBS.Text))
+               {
+                    MessageBox.Show("Bạn chưa nhập mã", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtMaBS.Focus();
+                    return false;
+               }
+
+               if (string.IsNullOrEmpty(txtHoTenBS.Text))
+               {
+                    MessageBox.Show("Bạn chưa nhập họ tên", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtHoTenBS.Focus();
+                    return false;
+               }
+
+               if (string.IsNullOrEmpty(dateBS.Text))
+               {
+                    MessageBox.Show("Bạn chưa nhập ngày tháng năm sinh", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    dateBS.Focus();
+                    return false;
+               }
+               if (string.IsNullOrEmpty(txtGTBS.Text))
+               {
+                    MessageBox.Show("Bạn chưa nhập giới tính", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtGTBS.Focus();
+                    return false;
+               }
+               if (string.IsNullOrEmpty(txtTrinhDo.Text))
+               {
+                    MessageBox.Show("Bạn chưa nhập trình độ", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtTrinhDo.Focus();
+                    return false;
+               }
+               if (string.IsNullOrEmpty(txtSDTBS.Text))
+               {
+                    MessageBox.Show("Bạn chưa nhập số điện thoại", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtSDTBS.Focus();
+                    return false;
+               }
+               return true;
+          }
+          public void clear()
+          {
+               txtMaBS.Text = "";
+               txtHoTenBS.Text = "";
+               txtGTBS.Text = "";
+               dateBS.Text = "";
+               dateBS1.Text = "";
+               txtTrinhDo.Text = "";
+               txtSDTBS.Text = "";
+          }
+          private void btnThemBS_Click(object sender, EventArgs e)
+          {
+               mo();
+               flag = "add";
+               clear();
+               TaoMaBSMoi();
+          }
+
+          private void btnSuaBS_Click(object sender, EventArgs e)
+          {
+               mo();
+               txtMaBS.ReadOnly = true;
+               flag = "edit";
+          }
+
+          private void btnXoaBS_Click(object sender, EventArgs e)
+          {
+               mo();
+               flag = "delete";
+               layoutThongTinBS.Enabled = false;
+          }
+
+          private void btnLuuBS_Click(object sender, EventArgs e)
+          {
+               if (check() == false)
+               {
+                    XtraMessageBox.Show("Bạn điền thông tin chưa đầy đủ");
+                    return;
+               }
+               KetNoi kn = new KetNoi();
+               if (flag == "add")
+               {
+                    if (kn.LoadDataHaveID("BacSiBietMa", "@MaBS", txtMaBS.Text).Rows.Count == 1)
+                         XtraMessageBox.Show("Mã bác sĩ đã có trong danh sách");
+                    else
+                    {
+                         kn.OpenConnection();
+                         kn.BacSi("ThemBS", txtMaBS.Text, txtHoTenBS.Text, dateBS.Text, txtGTBS.Text, txtTrinhDo.Text, txtSDTBS.Text);
+                    }
+               }
+               if (flag == "edit")
+               {
+                    if (kn.LoadDataHaveID("BacSiBietMa", "@MaBS", txtMaBS.Text).Rows.Count == 0)
+                         XtraMessageBox.Show("Không tìm thấy mã bác sĩ để sửa ");
+                    else
+                    {
+                         kn.OpenConnection();
+                         kn.BacSi("SuaBS", txtMaBS.Text, txtHoTenBS.Text, dateBS.Text, txtGTBS.Text, txtTrinhDo.Text, txtSDTBS.Text);
+                    }
+               }
+               if (flag == "delete")
+               {
+                    if (kn.LoadDataHaveID("BacSiBietMa", "@MaBS", txtMaBS.Text).Rows.Count == 0)
+                         XtraMessageBox.Show("Không tìm thấy mã bác sĩ để xóa ");
+                    else
+                    {
+                         kn.OpenConnection();
+                         kn.Xoa("XoaBS", "@MaBS", txtMaBS.Text);
+                         clear();
+                    }
+               }
+               getData();
+          }
+
+          private void dataBacSi_MouseClick(object sender, MouseEventArgs e)
+          {
+               
+                    int index = dataBacSi.CurrentRow.Index;
+                    txtMaBS.Text = dataBacSi.Rows[index].Cells[1].Value.ToString();
+                    txtHoTenBS.Text = dataBacSi.Rows[index].Cells[2].Value.ToString();
+                    dateBS.Text = dataBacSi.Rows[index].Cells[4].Value.ToString();
+                    txtGTBS.Text = dataBacSi.Rows[index].Cells[6].Value.ToString();
+                    txtTrinhDo.Text = dataBacSi.Rows[index].Cells[5].Value.ToString();
+                    txtSDTBS.Text = dataBacSi.Rows[index].Cells[3].Value.ToString();
+          }
+
+          private void btnBatDauTKBS_Click(object sender, EventArgs e)
+          {
+               mo();
+               clear();
+          }
+
+          private void btnTimKiemBS_Click(object sender, EventArgs e)
+          {
+               KetNoi kn = new KetNoi();
+               kn.OpenConnection();
+               dataBacSi.DataSource = kn.LoadDataSearch("TimKiemBS", txtMaBS.Text, txtHoTenBS.Text, dateBS.Text, dateBS1.Text, txtGTBS.Text, txtTrinhDo.Text, txtSDTBS.Text);
+               for (int i = 0; i < dataBacSi.Rows.Count - 1; i++)
+               {
+                    dataBacSi.Rows[i].Cells[0].Value = i + 1;
+               }
+          }
+
+          private void btnHuyBS_Click(object sender, EventArgs e)
+          {
+               khoa();
+          }
+
+          private void btnXuatExcelBN_Click(object sender, EventArgs e)
+          {
+               KetNoi kn = new KetNoi();
+               DataTable dt = kn.LoadData("XuatDanhSachBS");
+               if (dt.Rows.Count == 0)
+                    XtraMessageBox.Show("Không có dữ liệu để xuất");
+               else
+               {
+                    kn.OpenConnection();
+                    kn.LoadDataSet("XuatDanhSachBS").WriteXml(@"E:\Danh sách bác sĩ.xls");
+                    XtraMessageBox.Show("Xuất thành công");
+               }
+          }
+
+          private void btnXuatBCBS_Click(object sender, EventArgs e)
+          {
+               KetNoi kn = new KetNoi();
+               DataTable dt = kn.LoadDataHaveID("XuatBS","@MaBS",txtMaBS.Text);
+               if (dt.Rows.Count == 0)
+               {
+                    XtraMessageBox.Show("Không có dữ liệu để xuất");
+               }
+               //else
+               //{
+               //     XtraReportBS reportBS = new XtraReportBS();
+               //     reportBS.DataSource = dt;
+               //     reportBS.ShowPreviewDialog();
+               //}
+          }
+     }
+}
---
+++ code/FormYTa.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace DXApplication2
+{
+     partial class FormYTa
+     {
+          /// <summary>
+          /// Required designer variable.
+          /// </summary>
+          private System.ComponentModel.IContainer components = null;
+
+          /// <summary>
+          /// Clean up any resources being used.
+          /// </summary>
+          /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+          protected override void Dispose(bool disposing)
+          {
+               if (disposing && (components != null))
+               {
+                    components.Dispose();
+               }
+               base.Dispose(disposing);
+          }
+
+          #region Windows Form Designer generated code
+
+          /// <summary>
+          /// Required method for Designer support - do not modify
+          /// the contents of this method with the code editor.
+          /// </summary>
+          private void InitializeComponent()
+          {
+               this.dataGridView1 = new System.Windows.Forms.DataGridView();
+               this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+               this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+               this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+               this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+               this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+               ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+               ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+               this.layoutControl1.SuspendLayout();
+               ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+               ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+               ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+               ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+               this.SuspendLayout();
+               // 
+               // dataGridView1
+               // 
+               this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+               this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+               this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+               this.dataGridView1.GridColor = System.Drawing.SystemColors.ControlLightLight;
+               this.dataGridView1.Location = new System.Drawing.Point(60, 61);
+               this.dataGridView1.Name = "dataGridView1";
+               this.dataGridView1.RowHeadersWidth = 51;
+               this.dataGridView1.RowTemplate.Height = 24;
+               this.dataGridView1.Size = new System.Drawing.Size(927, 927);
+               this.dataGridView1.TabIndex = 0;
+               // 
+               // Column1
+               // 
+               this.Column1.HeaderText = "STT";
+               this.Column1.MinimumWidth = 6;
+               this.Column1.Name = "Column1";
+               this.Column1.Width = 75;
+               // 
+               // Column2
+               // 
+               this.Column2.HeaderText = "Mã y tá";
+               this.Column2.MinimumWidth = 6;
+               this.Column2.Name = "Column2";
+               this.Column2.Width = 105;
+               // 
+               // Column3
+               // 
+               this.Column3.HeaderText = "Họ tên y tá";
+               this.Column3.MinimumWidth = 6;
+               this.Column3.Name = "Column3";
+               this.Column3.Width = 185;
+               // 
+               // Column4
+               // 
+               this.Column4.HeaderText = "Ngày sinh";
+               this.Column4.MinimumWidth = 6;
+               this.Column4.Name = "Column4";
+               this.Column4.Width = 105;
+               // 
+               // Column5
+               // 
+               this.Column5.HeaderText = "Giới tính";
+               this.Column5.MinimumWidth = 6;
+               this.Column5.Name = "Column5";
+               this.Column5.Width = 95;
+               // 
+               // Column6
+               // 
+               this.Column6.HeaderText = "SĐT";
+               this.Column6.MinimumWidth = 6;
+               this.Column6.Name = "Column6";
+               this.Column6.Width = 87;
+               // 
+               // layoutControl1
+               // 
+               this.layoutControl1.Controls.Add(this.textEdit1);
+               this.layoutControl1.Location = new System.Drawing.Point(1113, 597);
+               this.layoutControl1.Name = "layoutControl1";
+               this.layoutControl1.Root = this.Root;
+               this.layoutControl1.Size = new System.Drawing.Size(778, 245);
+               this.layoutControl1.TabIndex = 1;
+               this.layoutControl1.Text = "layoutControl1";
+               // 
+               // Root
+               // 
+               this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+               this.Root.GroupBordersVisible = false;
+               this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlGroup1});
+               this.Root.Name = "Root";
+               this.Root.Size = new System.Drawing.Size(778, 245);
+               // 
+               // textEdit1
+               // 
+               this.textEdit1.Location = new System.Drawing.Point(144, 50);
+               this.textEdit1.Name = "textEdit1";
+               this.textEdit1.Size = new System.Drawing.Size(610, 22);
+               this.textEdit1.StyleController = this.layoutControl1;
+               this.textEdit1.TabIndex = 4;
+               // 
+               // layoutControlItem1
+               // 
+               this.layoutControlItem1.Control = this.textEdit1;
+               this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+               this.layoutControlItem1.Name = "layoutControlItem1";
+               this.layoutControlItem1.Size = new System.Drawing.Size(734, 175);
+               this.layoutControlItem1.TextSize = new System.Drawing.Size(108, 16);
+               // 
+               // layoutControlGroup1
+               // 
+               this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1});
+               this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
+               this.layoutControlGroup1.Name = "layoutControlGroup1";
+               this.layoutControlGroup1.Size = new System.Drawing.Size(758, 225);
+               // 
+               // FormYTa
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+               this.ClientSize = new System.Drawing.Size(1902, 1033);
+               this.Controls.Add(this.layoutControl1);
+               this.Controls.Add(this.dataGridView1);
+               this.Name = "FormYTa";
+               this.Text = "FormYTa";
+               this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+               ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+               ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+               this.layoutControl1.ResumeLayout(false);
+               ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+               ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+               ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+               ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+               this.ResumeLayout(false);
+
+          }
+
+          #endregion
+
+          private System.Windows.Forms.DataGridView dataGridView1;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+          private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+          private DevExpress.XtraLayout.LayoutControl layoutControl1;
+          private DevExpress.XtraLayout.LayoutControlGroup Root;
+          private DevExpress.XtraEditors.TextEdit textEdit1;
+          private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+          private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+     }
+}
